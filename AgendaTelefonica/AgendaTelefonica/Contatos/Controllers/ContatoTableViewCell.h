495b9230eaac9a935f77f6f17ed5a5496c65ea61
João Felipe Maciel <joaofelipemaciel@Mac-mini-de-Fundacao-4.local>
2018-07-30 17:58:26 -0300

    Ajuste login usuario + inclusao da contatosViewCell

---
+++ AgendaTelefonica/AgendaTelefonica/Contatos/Controllers/ContatoTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ContatoTableViewCell.h
+//  AgendaTelefonica
+//
+//  Created by João Felipe Maciel on 30/07/2018.
+//  Copyright © 2018 João Felipe Maciel de Brito Barros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContatoTableViewCell : UITableViewCell
+
+@end
---
+++ AgendaTelefonica/AgendaTelefonica/Contatos/Controllers/ContatoTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ContatoTableViewCell.m
+//  AgendaTelefonica
+//
+//  Created by João Felipe Maciel on 30/07/2018.
+//  Copyright © 2018 João Felipe Maciel de Brito Barros. All rights reserved.
+//
+
+#import "ContatoTableViewCell.h"
+
+@implementation ContatoTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
